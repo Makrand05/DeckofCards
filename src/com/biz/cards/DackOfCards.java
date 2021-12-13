ac000af97515ca59962eae86fbe51f755a189d56
Makrand <makrandshingare@gmail.com>
2021-12-13 11:18:34 +0530

    Added: Added Class for cards

--- src/com/biz/cards/DackOfCards.java
+++ src/com/biz/cards/DackOfCards.java
@@ -2,4 +2,14 @@ package com.biz.cards;
 
 public class DackOfCards {
 
+
+    public void showAllCards() {
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <13; j++) {
+                Card card=new Card(j,i);
+                System.out.println(card);
+            }
+        }
+    }
 }
---
+++ src/com/biz/cards/Main.java
@@ -0,0 +1,9 @@
+package com.biz.cards;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DackOfCards dackOfCards = new DackOfCards();
+        dackOfCards.showAllCards();
+    }
+}
